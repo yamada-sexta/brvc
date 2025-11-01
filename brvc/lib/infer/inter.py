8f69b4546e7aa0f1892ca8a115faadbffd2b03a2
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2025-11-01 12:08:13 -0500

    Create inter.py

---
+++ brvc/lib/infer/inter.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+def run_inference(
+    g_path: Path,
+):
+    pass
+
+
+def main():
+    from tap import tapify
+
+    tapify(run_inference)
+
+
+if __name__ == "__main__":
+    main()
