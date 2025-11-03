3efb6e30574f4f7ebad2c81b617f67c615960760
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2025-11-02 22:35:41 -0600

    Split files

--- brvc/lib/train/utils/dataset.py
+++ brvc/lib/train/utils/dataset.py
@@ -126,7 +126,7 @@ class TextAudioLoaderMultiNSFsid(torch.utils.data.Dataset):
         pitchf = torch.FloatTensor(pitchf)
         return phone, pitch, pitchf
 
-    def get_audio(self, filename: str) -> tuple[torch.Tensor, torch.Tensor]:
+    def get_audio(self, filename: str) -> tuple[torch.FloatTensor, torch.FloatTensor]:
         audio, sampling_rate = load_wav_to_torch(filename)
         if sampling_rate != self.sampling_rate:
             raise ValueError(
@@ -172,107 +172,10 @@ class TextAudioLoaderMultiNSFsid(torch.utils.data.Dataset):
         return spec, audio_norm
 
     def __getitem__(self, index: int) -> tuple[
-        torch.FloatTensor, torch.FloatTensor, torch.FloatTensor, torch.FloatTensor, torch.FloatTensor, torch.LongTensor
+        torch.FloatTensor, torch.FloatTensor, torch.FloatTensor, torch.LongTensor, torch.FloatTensor, torch.LongTensor
     ]:
         return self.get_audio_text_pair(self.audiopaths_and_text[index])
 
     def __len__(self):
         return len(self.audiopaths_and_text)
 
-
-class TextAudioCollateMultiNSFsid:
-    """Zero-pads model inputs and targets"""
-
-    def __init__(self, return_ids: bool = False):
-        self.return_ids = return_ids
-
-    def __call__(
-        self,
-        batch: list[
-            tuple[
-                torch.Tensor,
-                torch.Tensor,
-                torch.Tensor,
-                torch.Tensor,
-                torch.Tensor,
-                torch.LongTensor,
-            ]
-        ],
-    ) -> tuple[
-        FloatTensor,
-        LongTensor,
-        LongTensor,
-        FloatTensor,
-        FloatTensor,
-        LongTensor,
-        FloatTensor,
-        LongTensor,
-        LongTensor,
-    ]:
-        """Collate's training batch from normalized text and audio lengths
-        PARAMS
-        ------
-        batch: [text_normalized, spec_normalized, wav_normalized]
-        """
-        # Right zero-pad all one-hot text sequences to max input length
-        _, ids_sorted_decreasing = torch.sort(
-            torch.LongTensor([x[0].size(1) for x in batch]), dim=0, descending=True
-        )
-
-        max_spec_len = max([x[0].size(1) for x in batch])
-        max_wave_len = max([x[1].size(1) for x in batch])
-        spec_lengths = torch.LongTensor(len(batch))
-        wave_lengths = torch.LongTensor(len(batch))
-        spec_padded = torch.FloatTensor(len(batch), batch[0][0].size(0), max_spec_len)
-        wave_padded = torch.FloatTensor(len(batch), 1, max_wave_len)
-        spec_padded.zero_()
-        wave_padded.zero_()
-
-        max_phone_len = max([x[2].size(0) for x in batch])
-        phone_lengths = torch.LongTensor(len(batch))
-        phone_padded = torch.FloatTensor(
-            len(batch), max_phone_len, batch[0][2].shape[1]
-        )  # (spec, wav, phone, pitch)
-        pitch_padded = torch.LongTensor(len(batch), max_phone_len)
-        pitchf_padded = torch.FloatTensor(len(batch), max_phone_len)
-        phone_padded.zero_()
-        pitch_padded.zero_()
-        pitchf_padded.zero_()
-        # dv = torch.FloatTensor(len(batch), 256)#gin=256
-        sid = torch.LongTensor(len(batch))
-
-        for i in range(len(ids_sorted_decreasing)):
-            row = batch[ids_sorted_decreasing[i]]
-
-            spec = row[0]
-            spec_padded[i, :, : spec.size(1)] = spec
-            spec_lengths[i] = spec.size(1)
-
-            wave = row[1]
-            wave_padded[i, :, : wave.size(1)] = wave
-            wave_lengths[i] = wave.size(1)
-
-            phone = row[2]
-            phone_padded[i, : phone.size(0), :] = phone
-            phone_lengths[i] = phone.size(0)
-
-            pitch = row[3]
-            pitch_padded[i, : pitch.size(0)] = pitch
-            pitchf = row[4]
-            pitchf_padded[i, : pitchf.size(0)] = pitchf
-
-            # dv[i] = row[5]
-            sid[i] = row[5]
-
-        return (
-            phone_padded,
-            phone_lengths,
-            pitch_padded,
-            pitchf_padded,
-            spec_padded,
-            spec_lengths,
-            wave_padded,
-            wave_lengths,
-            # dv
-            sid,
-        )
